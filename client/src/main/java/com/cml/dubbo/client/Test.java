6d8ce6db833112b4c8ed7ba7722435ed3ca9d772
admin <865517964@qq.com>
2017-06-24 23:03:20 +0800

    update

--- client/src/main/java/com/cml/dubbo/client/Test.java
+++ client/src/main/java/com/cml/dubbo/client/Test.java
@@ -9,10 +9,22 @@ public class Test {
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "application.xml" });
 		context.start();
-		
+
+		System.out.println("start success!!");
 		Provider demoService = (Provider) context.getBean("demoService"); // 获取bean
+		System.out.println("====getbean======");
 		String message = "";
 		try {
+			message = demoService.build("2016-10-20");
+			System.out.println(" the message from server is:" + message);
+			message = demoService.build("2016-10-20");
+			System.out.println(" the message from server is:" + message);
+			message = demoService.build("2016-10-20");
+			System.out.println(" the message from server is:" + message);
+			message = demoService.build("2016-10-20");
+			System.out.println(" the message from server is:" + message);
+			message = demoService.build("2016-10-20");
+			System.out.println(" the message from server is:" + message);
 			message = demoService.build("2016-10-20");
 			System.out.println(" the message from server is:" + message);
 		} catch (Exception e) {
